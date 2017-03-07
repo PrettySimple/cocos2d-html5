6b22c71151f832923a50c8453cb692425bd9518f
Clément <clement@MACCLEMENT.local>
2017-03-07 13:34:18 +0100

    CCControl: small fix

--- extensions/gui/control-extension/CCControl.js
+++ extensions/gui/control-extension/CCControl.js
@@ -44,7 +44,7 @@ cc.CONTROL_STATE_NORMAL = 1 << 0; // The normal; or default state of a controlæ¢
 cc.CONTROL_STATE_HIGHLIGHTED = 1 << 1; // Highlighted state of a control. A control enters this state when a touch down; drag inside or drag enter is performed. You can retrieve and set this value through the highlighted property.
 cc.CONTROL_STATE_DISABLED = 1 << 2; // Disabled state of a control. This state indicates that the control is currently disabled. You can retrieve and set this value through the enabled property.
 cc.CONTROL_STATE_SELECTED = 1 << 3;  // Selected state of a control. This state indicates that the control is currently selected. You can retrieve and set this value through the selected property.
-cc.CONTROL_STATE_INITIAL = 1 << 3;
+cc.CONTROL_STATE_INITIAL = 1 << 0;
 
 /**
  * CCControl is inspired by the UIControl API class from the UIKit library of
