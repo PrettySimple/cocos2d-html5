ef6fae021dc2f2443497b18a4409f127ebc55f3d
pandamicro <lphuabin@gmail.com>
2017-05-09 11:32:50 +0800

    Use imagePool to reduce image memory usage in WebGL mode

--- cocos2d/core/textures/TexturesWebGL.js
+++ cocos2d/core/textures/TexturesWebGL.js
@@ -460,11 +460,8 @@ cc._tmp.WebGLTexture2D = function () {
             // Not sure about this ! Some texture need to be updated even after loaded
             if (!cc.game._rendererInitialized)
                 return;
-            if (!self._htmlElementObj) {
-                var img = cc.loader.getRes(self.url);
-                if (!img) return;
-                self.initWithElement(img);
-            }
+            if (!self._htmlElementObj)
+                return;
             if (!self._htmlElementObj.width || !self._htmlElementObj.height)
                 return;
 
@@ -501,6 +498,7 @@ cc._tmp.WebGLTexture2D = function () {
 
             self._hasPremultipliedAlpha = premultiplied;
             self._hasMipmaps = false;
+            self._htmlElementObj = null;
 
             //dispatch load event to listener.
             self.dispatchEvent("load");
@@ -859,7 +857,7 @@ cc._tmp.WebGLTextureAtlas = function () {
 cc._tmp.WebGLTextureCache = function () {
     var _p = cc.textureCache;
 
-    _p.handleLoadedTexture = function (url) {
+    _p.handleLoadedTexture = function (url, img) {
         var locTexs = this._textures, tex, ext;
         //remove judge(webgl)
         if (!cc.game._rendererInitialized) {
@@ -870,6 +868,7 @@ cc._tmp.WebGLTextureCache = function () {
             tex = locTexs[url] = new cc.Texture2D();
             tex.url = url;
         }
+        tex.initWithElement(img);
         ext = cc.path.extname(url);
         if (ext === ".png") {
             tex.handleLoadedTexture(true);
